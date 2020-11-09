29a2336e162ebb527a10c63796402fde2392e8b8
神楽坂らせん <kagurazakarasen@gmail.com>
2020-11-09 21:10:44 +0900

    空白方向をカーソルで選ぼうとすると"0"が表示されてしまうバグ対処
    ついでに、今までdrawStringしていた消し部分をdrawCharに変更。

--- NumberTron/NumberTron.ino
+++ NumberTron/NumberTron.ino
@@ -242,14 +242,18 @@ void loop() {
       while(c){
         ox=NowX;
         oy=NowY;
-        tft.drawString("  ", (MyX+ox)*TEXT_WIDTH, (MyY+oy)*TEXT_HEIGHT, 2); //Char(一文字）だとゴミが残る
+        //tft.drawString("  ", (MyX+ox)*TEXT_WIDTH, (MyY+oy)*TEXT_HEIGHT, 2); //Char(一文字）だとゴミが残る
+        tft.drawChar( (MyX+ox)*TEXT_WIDTH, (MyY+oy)*TEXT_HEIGHT,' ',ILI9341_GREEN, ILI9341_BLACK, 2);
         delay(200);
         //tft.drawNumber(STAGE[MyX+ox][MyY+oy], (MyX+ox)*TEXT_WIDTH, (MyY+oy)*TEXT_HEIGHT, 2); // Draw the Number
-        tft.drawChar( (MyX+ox)*TEXT_WIDTH, (MyY+oy)*TEXT_HEIGHT,STAGE[MyX+ox][MyY+oy]+48,ILI9341_GREEN, ILI9341_BLACK, 2);
+        if(STAGE[MyX+ox][MyY+oy]>0){ // 正の数なら（数字が入っていたら）
+          tft.drawChar( (MyX+ox)*TEXT_WIDTH, (MyY+oy)*TEXT_HEIGHT,STAGE[MyX+ox][MyY+oy]+48,ILI9341_GREEN, ILI9341_BLACK, 2);
+        }
 
         playTone(1014, 100); 
         c = buttonChk();
-        tft.drawString("  ", (MyX)*TEXT_WIDTH, (MyY)*TEXT_HEIGHT, 2); //Char(一文字）だとゴミが残る
+        //tft.drawString("  ", (MyX)*TEXT_WIDTH, (MyY)*TEXT_HEIGHT, 2); //Char(一文字）だとゴミが残る
+        tft.drawChar( (MyX)*TEXT_WIDTH, (MyY)*TEXT_HEIGHT,' ',ILI9341_GREEN, ILI9341_BLACK, 2);
       }
       //Serial.print("C Loop Out   ");
       Score=Score+STAGE[MyX+ox][MyY+oy];
